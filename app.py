a523b484296cd219fa7fcafefce1399ab0f06926
Sola Aremu Oluwadara <solaaremuoluwadara@gmail.com>
2019-07-15 01:53:21 +0100

    Updated Procfiles

--- app.py
+++ app.py
@@ -6,12 +6,11 @@ Created on Sat Jul 13 18:26:59 2019
 """
 
 from flask import Flask, render_template,url_for,request
-from flask_restful import reqparse, abort, Api, Resource
 import numpy as np
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
 from sklearn.externals import joblib
 from model import RainfallModel
+
+
 app = Flask(__name__)
 
 
@@ -40,7 +39,7 @@ def predict():
       year = request.form['year']
       year = np.reshape([[year]], (-1,1))
       rainfall= model.predict(year)
-    return render_template('result.html', prediction =rainfall, year =  request.form['year'])
+    return render_template('result.html', prediction =rainfall)
 
 
 if __name__ == '__main__':
